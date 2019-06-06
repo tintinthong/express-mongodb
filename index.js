c9f3d570552c7fd69008e5b9a272cddb895cc954
mingsterism <ming.k@hotmail.com>
2019-06-06 16:14:06 +0800

    feat(Multer): add multer for file upload

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-var fs=require('fs');
+var fs = require("fs");
 var path = require("path");
-var cors= require("cors");
-var request=require("request") // to use browser and download?
-const port=8080; 
-// funny when using port 8080 it works
-// when . using port 5501 it doesn't work. 
+var cors = require("cors");
+var request = require("request"); // to use browser and download?
 
-const mongodb= require('mongodb');
+var multer = require("multer");
+var upload = multer({ dest: "uploads/" });
+
+const port = 8080;
+// funny when using port 8080 it works
+// when . using port 5501 it doesn't work.
+
+const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
-const uri = "mongodb+srv://tintinthong:H-i5JBrKh-Xp3%21-@cluster0-duqpt.mongodb.net/test?retryWrites=true";
+const uri =
+  "mongodb+srv://tintinthong:H-i5JBrKh-Xp3%21-@cluster0-duqpt.mongodb.net/test?retryWrites=true";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
 // //handle cross origin requiues
@@ -31,12 +36,15 @@ const client = new MongoClient(uri, { useNewUrlParser: true });
 //     callback.onContinue();
 // };
 
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*'); // * => allow all origins
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,OPTIONS,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Accept'); // add remove headers according to your needs
-    next()
-})
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // * => allow all origins
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,OPTIONS,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, X-Auth-Token, Accept"
+  ); // add remove headers according to your needs
+  next();
+});
 
 // app.use(cors())
 
@@ -44,113 +52,104 @@ app.use(function (req, res, next) {
 let db;
 
 client.connect(err => {
-    if(err) {
-        console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
-    }
-    db= client.db("Cluster0");
-    
-    
-    
-})
-
-
-
-app.get('/', function(req, res) {
-    
-    res.sendFile(path.join(__dirname + '/index.html'));
+  if (err) {
+    console.log("Error occurred while connecting to MongoDB Atlas...\n", err);
+  }
+  db = client.db("Cluster0");
 });
 
-app.post('/postFile', (req, res) => {
-    
-    
-    
-    // const click = {clickTime: new Date()};
-    // console.log(click);
-    // console.log(db);
-    
-    // db.collection('clicks').save(click, (err, result) => {
-    //     if (err) {
-    //         return console.log(err);
-    //     }
-    //     console.log('click added to db');
-    //     res.sendStatus(201);
-    // });
-    
-    let stream = bucket = new mongodb.GridFSBucket(db);
-    fs.createReadStream('./upload/somevideo.mp4')
-    .pipe(bucket.openUploadStream('video.mp4'))
-    .on('error', function(error) {
-        assert.ifError(error);
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.get("/hey", (req, res) => {
+  res.send("HEYYY");
+});
+app.post("/postFile2", upload.single("avatar"), (req, res) => {
+  console.log("CREATING");
+  console.log(req.file);
+  //   console.log("the data is received");
+  //   res.json({ test: "haaa" });
+});
+
+app.get("/postFile2", (req, res) => {
+  console.log("GETTING POST FILE 2");
+  res.send("Heyy");
+});
+
+app.post("/postFile", (req, res) => {
+  // const click = {clickTime: new Date()};
+  // console.log(click);
+  // console.log(db);
+
+  // db.collection('clicks').save(click, (err, result) => {
+  //     if (err) {
+  //         return console.log(err);
+  //     }
+  //     console.log('click added to db');
+  //     res.sendStatus(201);
+  // });
+
+  let stream = (bucket = new mongodb.GridFSBucket(db));
+  fs.createReadStream("./upload/somevideo.mp4")
+    .pipe(bucket.openUploadStream("video.mp4"))
+    .on("error", function(error) {
+      assert.ifError(error);
     })
-    .on('finish', function() {
-        console.log('done!  resource  downloaded to server');
-        res.sendStatus(201);
-        // process.exit(0);
+    .on("finish", function() {
+      console.log("done!  resource  downloaded to server");
+      res.sendStatus(201);
+      // process.exit(0);
     });
-    
-    
 });
 
+app.get("/getFile", function(req, res) {
+  var bucket = new mongodb.GridFSBucket(db);
+  let stream = bucket
+    .openDownloadStreamByName("video.mp4")
+    .pipe(fs.createWriteStream("./download/output.mp4"))
+    .on("error", function(error) {
+      assert.ifError(error);
+    })
+    .on("finish", function() {
+      console.log("done!");
 
-app.get('/getFile', function(req,res){
-    
-    
-    var bucket = new mongodb.GridFSBucket(db);
-    let stream = bucket.openDownloadStreamByName('video.mp4').
-    pipe(fs.createWriteStream('./download/output.mp4')).
-    on('error', function(error) {
-        assert.ifError(error);
-    }).
-    on('finish', function() {
-        console.log('done!');
-      
-        let pathToFile =path.join(__dirname, 'output.mp4')
-        res.sendFile(pathToFile);
-        res.sendStatus(201);
-        // process.exit(0);
+      let pathToFile = path.join(__dirname, "output.mp4");
+      res.sendFile(pathToFile);
+      res.sendStatus(201);
+      // process.exit(0);
     });
 
-    // //send  stream 
-    // res.send(stream).on('finish',function(){
-    //     res.end('ended')
-    // });
-    //https://stackoverflow.com/questions/25463423/res-sendfile-absolute-path
-
-
-    
-    
+  // //send  stream
+  // res.send(stream).on('finish',function(){
+  //     res.end('ended')
+  // });
+  //https://stackoverflow.com/questions/25463423/res-sendfile-absolute-path
 });
 
-app.get('/downloadFile', function(req,res){
-    
-    const file = `${__dirname}/upload/somevideo.mp4`;
-    res.download(file, ()=>{
-        console.log("file downloaded")
-    });
-    
-    var fileContents = Buffer.from(fileData, "base64");
-    
-    var readStream = new stream.PassThrough();
-    readStream.end(fileContents);
-    
-    response.set('Content-disposition', 'attachment; filename=' + fileName);
-    response.set('Content-Type', 'text/plain');
-    
-    readStream.pipe(res);
-    
-    
+app.get("/downloadFile", function(req, res) {
+  const file = `${__dirname}/upload/somevideo.mp4`;
+  res.download(file, () => {
+    console.log("file downloaded");
+  });
+
+  var fileContents = Buffer.from(fileData, "base64");
+
+  var readStream = new stream.PassThrough();
+  readStream.end(fileContents);
+
+  response.set("Content-disposition", "attachment; filename=" + fileName);
+  response.set("Content-Type", "text/plain");
+
+  readStream.pipe(res);
 });
 
-
-app.listen(port, ()=> console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 //https://stackoverflow.com/questions/45812943/how-to-stream-from-nodejs-server-to-client
 //https://stackoverflow.com/questions/45922074/node-express-js-download-file-from-memory-filename-must-be-a-string
 //https://stackoverflow.com/questions/30985596/issue-with-downloading-a-file-from-gridfs-in-mongodb-nodejs-gridfs-stack
 
-
-
 // app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
 
 // https://gist.github.com/aerrity/fd393e5511106420fba0c9602cc05d35
