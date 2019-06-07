350433d8ee951381a58f05b1d66e21be458aa9a6
mingsterism <ming.k@hotmail.com>
2019-06-07 13:47:26 +0800

    docs(Documentation microservice):

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-var fs = require("fs");
 var path = require("path");
 var cors = require("cors");
 var request = require("request"); // to use browser and download?
@@ -8,10 +7,10 @@ var request = require("request"); // to use browser and download?
 var Busboy = require("busboy");
 
 const port = 8080;
-// funny when using port 8080 it works
-// when . using port 5501 it doesn't work.
 
 const mongodb = require("mongodb");
+const ObjectID = require("mongodb").ObjectID;
+
 const MongoClient = mongodb.MongoClient;
 const uri =
   "mongodb+srv://tintinthong:H-i5JBrKh-Xp3%21-@cluster0-duqpt.mongodb.net/test?retryWrites=true";
@@ -61,24 +60,11 @@ app.get("/", function(req, res, bucket) {
   res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-app.get("/hey", (req, res, bucket) => {
-  res.send("HEYYY");
-});
-
-app.get("/myFile", (req, res) => {
-  let pathToFile = path.join(__dirname, "output.mp4");
-  console.log(pathToFile);
-  res.download(pathToFile);
-});
-
-app.post("/postFile2", (req, res, bucket) => {
+app.post("/postFile", (req, res, bucket) => {
   var bucket = new mongodb.GridFSBucket(db);
-  console.log("bucket", bucket);
-  console.log(req.headers);
   const busboy = new Busboy({ headers: req.headers });
   req.pipe(busboy);
   busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
-    console.log("FILENAME: ", filename);
     file
       .pipe(bucket.openUploadStream(filename))
       .on("error", err => {
@@ -89,109 +75,32 @@ app.post("/postFile2", (req, res, bucket) => {
       });
   });
 
-  //     file.on("data", data => {
-  //       data.pipe(bucket.openUploadStream("file101"));
-  //       console.log("DATA");
-  //       console.log(data);
-  //     });
-  //     // Completed streaming the file.
-  //     file.on("end", function() {
-  //       console.log("Finished with " + fieldname);
-  //     });
-  //   });
   // Listen for event when Busboy is finished parsing the form.
   busboy.on("finish", function() {
     res.statusCode = 200;
-    res.send("BOOO");
-    res.end();
+    res.json({ statusCode: 200 });
   });
 
   // Pipe the HTTP Request into Busboy.
 });
 
-app.post("/postFile", (req, res) => {
-  // const click = {clickTime: new Date()};
-  // console.log(click);
-  // console.log(db);
-
-  // db.collection('clicks').save(click, (err, result) => {
-  //     if (err) {
-  //         return console.log(err);
-  //     }
-  //     console.log('click added to db');
-  //     res.sendStatus(201);
-  // });
-
-  let stream = (bucket = new mongodb.GridFSBucket(db));
-  fs.createReadStream("./upload/somevideo.mp4")
-    .pipe(bucket.openUploadStream("video.mp4"))
-    .on("error", function(error) {
-      assert.ifError(error);
-    })
-    .on("finish", function() {
-      console.log("done!  resource  downloaded to server");
-      res.sendStatus(201);
-      // process.exit(0);
+app.get("/listFiles", (req, res) => {
+  var bucket = new mongodb.GridFSBucket(db);
+  bucket
+    .find({})
+    .toArray()
+    .then(data => {
+      console.log("Data", data);
+      res.send(data);
     });
 });
 
 app.get("/getFile", function(req, res) {
-  console.log(req.headers);
-  let pathToFile = path.join(__dirname, "output.mp4");
-  console.log(pathToFile);
-  res.download(pathToFile);
-});
-//   const file = `${__dirname}/upload/somevideo.mp4`;
-//   res.download(file, () => {
-//     console.log("file downloaded");
-//   });
-
-//   var bucket = new mongodb.GridFSBucket(db);
-//   let stream = bucket
-//     .openDownloadStreamByName("video.mp4")
-//     .pipe(fs.createWriteStream("./download/output.mp4"))
-//     .on("error", function(error) {
-//       assert.ifError(error);
-//     })
-//     .on("finish", function() {
-//       console.log("done!");
-
-//       let pathToFile = path.join(__dirname, "output.mp4");
-//       res.sendFile(pathToFile);
-//       res.sendStatus(201);
-//       // process.exit(0);
-//     });
-
-// //send  stream
-// res.send(stream).on('finish',function(){
-//     res.end('ended')
-// });
-//https://stackoverflow.com/questions/25463423/res-sendfile-absolute-path
-// });
-
-app.get("/downloadFile", function(req, res) {
-  const file = `${__dirname}/upload/somevideo.mp4`;
-  res.download(file, () => {
-    console.log("file downloaded");
-  });
-
-  //   var fileContents = Buffer.from(fileData, "base64");
-
-  //   var readStream = new stream.PassThrough();
-  //   readStream.end(fileContents);
-
-  //   response.set("Content-disposition", "attachment; filename=" + fileName);
-  //   response.set("Content-Type", "text/plain");
-
-  //   readStream.pipe(res);
+  const { id } = req.query;
+  var bucket = new mongodb.GridFSBucket(db);
+  const bucketFile = bucket.openDownloadStream(ObjectID(id));
+  bucketFile.pipe(res);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-//https://stackoverflow.com/questions/45812943/how-to-stream-from-nodejs-server-to-client
-//https://stackoverflow.com/questions/45922074/node-express-js-download-file-from-memory-filename-must-be-a-string
-//https://stackoverflow.com/questions/30985596/issue-with-downloading-a-file-from-gridfs-in-mongodb-nodejs-gridfs-stack
-
-// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-// https://gist.github.com/aerrity/fd393e5511106420fba0c9602cc05d35
